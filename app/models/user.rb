8a092d67e96318b526e8dc585465f395ed8e3dd3
AJ Guiles <ashiguiles@gmail.com>
2019-09-07 19:39:58 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  
+    def authenticate(password)
+   
+    salt = password_digest[0..28]
+    hashed = BCrypt::Engine::hash_secret(password, salt)
+    return nil unless (salt + hashed) == self.password_digest
+  end
+end
 end
